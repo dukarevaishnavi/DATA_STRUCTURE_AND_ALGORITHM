5a415e6ad95a7450a63cd8cd964c897862188784
Vaishnavi_Dukare <142210673+dukarevaishnavi@users.noreply.github.com>
2025-06-05 23:24:52 +0530

    Time: 0 ms (100%), Space: 41.6 MB (44.04%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix.length == 0) return result;
+
+        int start_row = 0;
+        int end_row = matrix.length - 1;
+        int start_column = 0;
+        int end_column = matrix[0].length - 1;
+
+        while (start_row <= end_row && start_column <= end_column) {
+            // Traverse top row
+            for (int j = start_column; j <= end_column; j++) {
+                result.add(matrix[start_row][j]);
+            }
+
+            // Traverse right column
+            for (int i = start_row + 1; i <= end_row; i++) {
+                result.add(matrix[i][end_column]);
+            }
+
+            // Traverse bottom row
+            if (start_row < end_row) {
+                for (int j = end_column - 1; j >= start_column; j--) {
+                    result.add(matrix[end_row][j]);
+                }
+            }
+
+            // Traverse left column
+            if (start_column < end_column) {
+                for (int i = end_row - 1; i > start_row; i--) {
+                    result.add(matrix[i][start_column]);
+                }
+            }
+
+            // Move to inner layer
+            start_row++;
+            end_row--;
+            start_column++;
+            end_column--;
+        }
+
+        return result;
+    }
+}
